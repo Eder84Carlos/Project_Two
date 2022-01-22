a8f99a019daeccf0483e9f49d8b78dbb1bae5de7
Eder Carvalho <91353220+Eder84Carlos@users.noreply.github.com>
2022-01-21 21:24:01 -0300

    Project Two

---
+++ src/project_two/Project_Two.java
@@ -0,0 +1,18 @@
+package project_two;
+
+public class Project_Two {
+
+	public static void main(String[] args) {
+		
+		int x, y;
+		
+		x = 5;
+		
+		y = 2 * x;
+		
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
